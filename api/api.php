1bd3a72d25a11008b924e4bd505c1638232cfefd
Roel <roel.van.bilzen@gmail.com>
2020-03-24 14:48:00 +0100

    Verwijdering van de beperking $uri_count

--- api/api.php
+++ api/api.php
@@ -2,7 +2,6 @@
 require_once '../lib/autoload.php';
 require_once  'access_control.php';
 $TaskService = $Container->getTaskService();
-$uri_count = 5;
 
 // Get URI parts
 $uri_parts = explode( "/", $_SERVER["REQUEST_URI"]);
@@ -15,7 +14,7 @@ $last_part = end( $uri_parts );
 $penultimate_part = $uri_parts[ $count - 2 ];
 
 // Execute function based on URI
-if ( $count == $uri_count AND $last_part == 'taken' ) {
+if ( $last_part == 'taken' ) {
     switch( $_SERVER['REQUEST_METHOD'] ) {
         case 'GET':
             $TaskService->read();
@@ -29,7 +28,7 @@ if ( $count == $uri_count AND $last_part == 'taken' ) {
     }
 }
 
-if ( $count == $uri_count + 1 AND $penultimate_part == "taak" ){
+if ( $penultimate_part == "taak" ){
     switch( $_SERVER['REQUEST_METHOD'] ) {
         case 'GET':
             $TaskService->read_single( $last_part );
